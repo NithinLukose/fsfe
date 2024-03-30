902af73c5d5ce6dbd74148779dd4e912b8fccbba
NithinLukose <nithinclukose12@gmail.com>
2024-03-30 11:20:50 +0530

    feat: add web socket

---
+++ index-ws.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+const server = http.createServer();
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+server.on("request", app);
+server.listen(3000, function () {
+  console.log("server listening at port 3000");
+});
+
+const WebSocketServer = require("ws").Server;
+
+const wss = new WebSocketServer({ server: server });
+
+wss.on("connection", function connection(ws) {
+  const numClients = wss.clients.size;
+
+  console.log("clients connected: ", numClients);
+
+  wss.broadcast(`Current visitors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send("welcome!");
+  }
+
+  ws.on("close", function close() {
+    wss.broadcast(`Current visitors: ${wss.clients.size}`);
+    console.log("A client has disconnected");
+  });
+
+  ws.on("error", function error() {
+    //
+  });
+});
+
+/**
+ * Broadcast data to all connected clients
+ * @param  {Object} data
+ * @void
+ */
+wss.broadcast = function broadcast(data) {
+  console.log("Broadcasting: ", data);
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
